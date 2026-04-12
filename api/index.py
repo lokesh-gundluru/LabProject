a53704fc82c03d1c1cd60431184055dcc82209a7
lokesh-gundluru <lokeshgundluru28@gmail.com>
2026-04-13 01:21:20 +0530

    fix

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
-programs = {
-    "1": "print('Program 1')",
-    "2": "print('Program 2')"
-}
-
 def handler(request):
+    programs = {
+        "1": "print('Program 1')",
+        "2": "print('Program 2')"
+    }
+
     path = request.path.split("/")[-1]
     code = programs.get(path, "Program not found")
 
@@ -12,3 +12,7 @@ def handler(request):
         "headers": {"Content-Type": "text/plain"},
         "body": code
     }
+
+
+# 👇 THIS LINE IS THE KEY (YOU MISSED THIS)
+app = handler
